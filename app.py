ac68f3045ca1fca4f493560807b034529120f1de
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-06-10 03:01:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,8 @@
 import streamlit as st
 import pandas as pd
-import joblib
-from sklearn.preprocessing import StandardScaler
+import requests
 
-# Load the model and scaler
-model = joblib.load('random_forest_model.joblib')
-scaler = joblib.load('scaler.joblib')
-
-def get_water_quality_prediction(user_data, model, scaler):
-    user_data = user_data.fillna(user_data.median())
-    user_data.columns = user_data.columns.str.lower().str.replace('.', '')
-    X = user_data
-    X = scaler.transform(X)
-    prediction = model.predict(X)
-    return prediction
-
-# Streamlit app
-st.title('Water Quality Prediction')
-
-st.markdown("""
-<style>
-.main {
-    background-color: #f0f2f6;
-}
-.reportview-container .markdown-text-container {
-    font-family: 'Arial', sans-serif;
-    color: #4c4c4c;
-}
-</style>
-""", unsafe_allow_html=True)
+st.title('AQUAMIND')
 
 st.sidebar.header('Input Features')
 temperature = st.sidebar.number_input("Temperature", min_value=0.0, step=0.1)
@@ -38,27 +12,27 @@ conductivity = st.sidebar.number_input("Conductivity", min_value=0.0, step=0.1)
 bod = st.sidebar.number_input("Biochemical Oxygen Demand (BOD)", min_value=0.0, step=0.1)
 
 user_data = {
-    'temperature': [temperature],
-    'do': [do],
-    'ph': [ph],
-    'conductivity': [conductivity],
-    'bod': [bod]
+    'temperature': temperature,
+    'do': do,
+    'ph': ph,
+    'conductivity': conductivity,
+    'bod': bod
 }
 
-input_df = pd.DataFrame(user_data)
-
 if st.sidebar.button('Predict Water Quality'):
-    prediction = get_water_quality_prediction(input_df, model, scaler)
-    quality = "Potable" if prediction[0] == 1 else "Not Potable"
-    st.subheader(f'Predicted Water Quality: {quality}')
-
-    if prediction[0] == 1:
-        st.success('The water is potable.')
+    response = requests.post("http://127.0.0.1:8000/predict", json=user_data)
+    if response.status_code == 200:
+        prediction = response.json()['quality']
+        st.subheader(f'Predicted Water Quality: {prediction}')
+        if prediction == "Potable":
+            st.success('The water is potable.')
+        else:
+            st.error('The water is not potable.')
     else:
-        st.error('The water is not potable.')
+        st.error(f"Error: {response.status_code}, {response.text}")
 
 st.markdown("""
-### Water Quality Prediction App
+### AQUAMIND
 This app predicts the quality of water based on various input features.
 Adjust the features in the sidebar and click 'Predict Water Quality' to see the prediction.
 """)
